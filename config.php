8c63fbbd12b54447c4d5777a1be9842bcc8913e2
Lucas Silva <lucs.silva11@gmail.com>
2025-06-15 19:18:13 -0300

    banco dois tipos

--- config.php
+++ config.php
@@ -10,3 +10,17 @@ if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 ?>
+
+<!-- Padrão sem senha e sem porta 3307 -->
+<!-- <?php
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "topcarros";
+
+$conn = new mysqli($host, $usuario, $senha, $banco, 3306);
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+?> -->
