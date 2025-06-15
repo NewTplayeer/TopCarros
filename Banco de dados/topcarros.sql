2b451d7ffe36b387f1ec7a5727afbf90122f24b1
NewT <brunotavaresdefreitas@gmail.com>
2025-06-15 18:16:04 -0300

    banco novo

--- Banco de dados/topcarros.sql
+++ Banco de dados/topcarros.sql
@@ -1,43 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Tempo de geração: 15/06/2025 às 23:12
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
 
-CREATE DATABASE IF NOT EXISTS topcarros;
-USE topcarros;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    tipo ENUM('admin', 'cliente') DEFAULT 'cliente'
-);
 
-INSERT INTO `usuarios` (`nome`, `email`, `senha`) VALUES
-('Bruno', 'brunotavaresdefreitas@gmail.com', '$2y$10$T7tdcoczlE4w6CAWQ/MXweU80gjcmOtMiq0xL5CUfXkSzEAuf.q3u'),
-('MaFee', 'Mafee@gmail.com', '$2y$10$2HzSZ7tp/lufewlG5WKzHuiEhqXf9hfPt/PjQXm3t9NryMSMWCxW.'),
-('Kerllon Gedyel', 't4kamoto@gmail.com', '$2y$10$UPdhjro3vkf6DN..fLkbt.0fcpG0AwZa9Z4W3Uoln9Zs/l1kO4N3O'),
-('Caio', 'caio@gmail.com', '$2y$10$f19I/AGSATcWZGf9ksYoyOqz0Yd5lMRPkqqS2mD7UysLGh1DCvlze'),
-('Lucas', 'Lucas@gmail.com', '$2y$10$/t6yJcLqcsMpZEUmP/SQDOT//.Q48/7u46yH1FnvNiX1Qt81DRhqC');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE marcas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL UNIQUE
-);
+--
+-- Banco de dados: `topcarros`
+--
 
-CREATE TABLE veiculos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    marca_id INT,
-    modelo VARCHAR(100),
-    ano INT,
-    preco DECIMAL(10,2),
-    imagem VARCHAR(255),
-    FOREIGN KEY (marca_id) REFERENCES marcas(id)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE anuncios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT,
-    veiculo_id INT,
-    data_postagem DATETIME DEFAULT CURRENT_TIMESTAMP,
-    status ENUM('ativo', 'vendido') DEFAULT 'ativo',
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-    FOREIGN KEY (veiculo_id) REFERENCES veiculos(id)
-);
+--
+-- Estrutura para tabela `anuncios`
+--
+
+CREATE TABLE `anuncios` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `veiculo_id` int(11) DEFAULT NULL,
+  `data_postagem` datetime DEFAULT current_timestamp(),
+  `status` enum('ativo','vendido') DEFAULT 'ativo'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `anuncios`
+--
+
+INSERT INTO `anuncios` (`id`, `usuario_id`, `veiculo_id`, `data_postagem`, `status`) VALUES
+(6, 2, 3, '2025-06-03 20:18:48', 'ativo');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `marcas`
+--
+
+CREATE TABLE `marcas` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `marcas`
+--
+
+INSERT INTO `marcas` (`id`, `nome`) VALUES
+(6, 'Audi'),
+(2, 'Ford'),
+(5, 'Honda'),
+(3, 'Porsche'),
+(4, 'Subaru');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo` enum('admin','cliente') DEFAULT 'cliente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `tipo`) VALUES
+(2, 'Bruno Tavares de Freitas', 'brunotavaresdefreitas@gmail.com', '$2y$10$0VQ7ZTOP/K0CQlAhAAup.OSiJzlICZsocGDgK3mIg2bx2QL4diNtm', 'admin'),
+(3, 'Gustavo Henrique de Oliveira Teles', '1gustavo1teles@gmail.com', '$2y$10$3PuDbmtR44PItMGCkAL5YOQ9lXSI1bpHlqvHYTVjpmEV6C8uwy8qG', 'cliente'),
+(4, 'Breno Viado', 'Breno@gmail.com', '$2y$10$hrb4KPtT4Kj0JiUugsjTi.1.2iiWmKuGLXDIjsgD.KExWhJ37dVDm', 'cliente'),
+(5, 'Maria Fernanda Prado Duque', 'MaFee@gmail.com', '$2y$10$h4xZoRp3sMdzgAUnINAR4.7l01QT7AJMSltpBXCM6xFZ.AGw3op2m', 'cliente');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `veiculos`
+--
+
+CREATE TABLE `veiculos` (
+  `id` int(11) NOT NULL,
+  `marca_id` int(11) DEFAULT NULL,
+  `modelo` varchar(100) DEFAULT NULL,
+  `ano` int(11) DEFAULT NULL,
+  `preco` decimal(10,2) DEFAULT NULL,
+  `imagem` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `veiculos`
+--
+
+INSERT INTO `veiculos` (`id`, `marca_id`, `modelo`, `ano`, `preco`, `imagem`) VALUES
+(2, 2, 'Mustang', 2018, 500000.00, 'MustangGT.jpg'),
+(3, 3, 'GT3RS', 2020, 2600000.00, 'PorscheGT3RS.jpg'),
+(6, 5, 'Honde Civic G6', 1986, 12000000.00, 'CivicEg6.jpg'),
+(7, 6, 'Audi A4', 2014, 30000000.00, 'AudiA4.jpg');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `anuncios`
+--
+ALTER TABLE `anuncios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `veiculo_id` (`veiculo_id`);
+
+--
+-- Índices de tabela `marcas`
+--
+ALTER TABLE `marcas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nome` (`nome`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Índices de tabela `veiculos`
+--
+ALTER TABLE `veiculos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `marca_id` (`marca_id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `anuncios`
+--
+ALTER TABLE `anuncios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `marcas`
+--
+ALTER TABLE `marcas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `veiculos`
+--
+ALTER TABLE `veiculos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `anuncios`
+--
+ALTER TABLE `anuncios`
+  ADD CONSTRAINT `anuncios_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `anuncios_ibfk_2` FOREIGN KEY (`veiculo_id`) REFERENCES `veiculos` (`id`);
+
+--
+-- Restrições para tabelas `veiculos`
+--
+ALTER TABLE `veiculos`
+  ADD CONSTRAINT `veiculos_ibfk_1` FOREIGN KEY (`marca_id`) REFERENCES `marcas` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
